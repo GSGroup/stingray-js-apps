61367e8ca0016e12974ff63fd5a0353bfd884c72
Vladimir Menshakov <menshakov@spb.gs.ru>
2013-11-01 16:10:59 +0400

    moved utils.js to controls

---
+++ controls/utils.js
@@ -0,0 +1,42 @@
+var Color = function(color) {
+	if (!((color.length == 4 || color.length == 7 || color.length == 9) && color[0] == "#"))
+		throw new Error("Invalid color presentation");
+	
+	if (color.length == 4) {
+		var newColor = "#";
+
+		for (var i = 1; i < color.length; ++i)
+			newColor += color[i] + color[i];
+
+		color = newColor;
+	}
+	
+	this.r = parseInt(color.substr(1, 2), 16);
+	this.g = parseInt(color.substr(3, 2), 16);
+	this.b = parseInt(color.substr(5, 2), 16);
+	this.a = color.length == 9 ? parseInt(color.substr(7, 2), 16) : 255;
+};
+
+Color.prototype.toString = function() {
+	var toHex = function (a) {
+		return ("0" + a.toString(16)).slice(-2);
+	}
+	return "#" + toHex(this.r) + toHex(this.g) + toHex(this.b) + toHex(this.a);
+};
+
+this.lighter = function (color, coeff) {
+	var c = new Color(color);
+
+	for (var i in c) {
+		if (i != 'a' && i != 'toString')
+			c[i] = Math.min(Math.floor(c[i] * coeff), 255);
+	}
+
+	return c.toString();
+}
+
+this.setAlpha = function (color, alpha) { // 0..255
+	var c = new Color(color);
+	c.a = alpha;
+	return c.toString();
+}
