ee034d9d44542742d75fc5b850f3b15f50e3a6d6
Vladimir Menshakov <menshakov@spb.gs.ru>
2015-10-05 13:19:47 +0300

    added russian translation for 2048

--- 2048/ru.ts
+++ 2048/ru.ts
@@ -1,32 +1,32 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
+<TS version="2.1" language="ru_RU" sourcelanguage="ru_RU">
 <context>
     <name>Game</name>
     <message>
         <location filename="Game.qml" line="158"/>
         <source>BEST</source>
-        <translation type="unfinished"></translation>
+        <translation>ЛУЧШИЙ</translation>
     </message>
     <message>
         <location filename="Game.qml" line="200"/>
         <source>SCORE</source>
-        <translation type="unfinished"></translation>
+        <translation>СЧЕТ</translation>
     </message>
     <message>
         <location filename="Game.qml" line="230"/>
         <source>GAME OVER</source>
-        <translation type="unfinished"></translation>
+        <translation>ИГРА ОКОНЧЕНА</translation>
     </message>
     <message>
         <location filename="Game.qml" line="245"/>
         <source>Try again</source>
-        <translation type="unfinished"></translation>
+        <translation>Попытайтесь ещё раз</translation>
     </message>
     <message>
         <location filename="Game.qml" line="322"/>
         <source>Use your arrow keys to move the tiles. When two tiles with the same number touch, they merge into one!</source>
-        <translation type="unfinished"></translation>
+        <translation>Используйте стрелки, чтобы двигать клетками. Когда две клетки встречаются, случается МЕЙОЗ!</translation>
     </message>
 </context>
 </TS>
