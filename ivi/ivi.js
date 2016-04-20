be995ad44f95f3a107dbdd63cf57a656bf60f514
Ivan Leonov <ivan.leonov@iksatprof.ru>
2016-04-20 18:15:54 +0300

    Small changes to run app on box

--- ivi/ivi.js
+++ ivi/ivi.js
@@ -1,7 +1,9 @@
-this.getPromoVideos = function () {
+this.getPromoVideos1 = function (url) {
     var request = new XMLHttpRequest();
     request.open("GET", "https://api.ivi.ru/mobileapi/videos/v5", false);
     request.send();
+    
+    log(url);
 
     if (request.readyState === XMLHttpRequest.DONE)
       if (request.status && request.status === 200)
