38092ffc26f384feb35ed2f1b970ed1ebcc0b846
Alexander Dikanchukov <aleksandr.dikanchukov@iksatprof.ru>
2016-04-28 17:35:29 +0300

    Final visual refactoring and fixes

--- ivi/js/constants.js
+++ ivi/js/constants.js
@@ -1,12 +1,19 @@
-this.categories = [{ title: "Комедии", url: "https://api.ivi.ru/mobileapi/videos/v5" },
-                   { title: "Фантастика", url: "https://api.ivi.ru/mobileapi/compilations/v5/" },
-                   { title: "Ужасы", url: "https://api.ivi.ru/mobileapi/compilations/v5/" },
-                   { title: "Боевики", url: "https://api.ivi.ru/mobileapi/compilations/v5/" },
-                   { title: "Мелодрамы", url: "https://api.ivi.ru/mobileapi/compilations/v5/" }];
+this.categories = [{ title: "Комедии", url: "https://api.ivi.ru/mobileapi/videos/v5/?genre=95" },
+                   { title: "Фантастика", url: "https://api.ivi.ru/mobileapi/videos/v5/?genre=166" },
+                   { title: "Ужасы", url: "https://api.ivi.ru/mobileapi/videos/v5/?genre=99" },
+                   { title: "Боевики", url: "https://api.ivi.ru/mobileapi/videos/v5/?genre=94" },
+                   { title: "Мелодрамы", url: "https://api.ivi.ru/mobileapi/videos/v5/?genre=107" }];
 
 this.colors = {"active" : "#EC174F",
-               "text" : "#91949C"};
+               "inactive" : "#91949C"};
 
-this.inactive_opacity = 0.8;
+this.poster = {"width"  : 172,
+               "height" : 264};
 
-this.border = 60;
+this.menuWidth = 256;
+
+this.inactiveOpacity = 0.5;
+
+this.animationDuration = 150;
+
+this.margin = 60;
