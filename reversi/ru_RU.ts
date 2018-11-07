7069b81e304aa974685db0ed6d4aaf155a39a6ca
Ivan Leonov <ivan.leonov@gs-labs.ru>
2018-11-07 13:25:49 +0300

    reversi: Fix translations
    	use 'tr' for correct .ts file constructing from literals

--- reversi/ru_RU.ts
+++ reversi/ru_RU.ts
@@ -4,116 +4,92 @@
 <context>
     <name>Game</name>
     <message>
-        <location filename="Game.qml" line="39"/>
-        <source>Reversi</source>
-        <translation>Реверси</translation>
-    </message>
-    <message>
-        <location filename="Game.qml" line="53"/>
-        <source>Start playing alone</source>
-        <translation>Играть одному</translation>
-    </message>
-    <message>
-        <location filename="Game.qml" line="54"/>
-        <source>Start playing together</source>
-        <translation>Играть вдвоем</translation>
-    </message>
-    <message>
-        <location filename="Game.qml" line="85"/>
-        <source>White</source>
-        <translation>Белые</translation>
-    </message>
-    <message>
-        <location filename="Game.qml" line="103"/>
         <source>Black</source>
         <translation>Черные</translation>
     </message>
     <message>
-        <location filename="Game.qml" line="122"/>
-        <source>You&apos;re playing with white</source>
-        <translation>Вы играете белыми</translation>
-    </message>
-    <message>
-        <location filename="Game.qml" line="122"/>
-        <source>You&apos;re playing with black</source>
-        <translation>Вы играете черными</translation>
-    </message>
-    <message>
-        <location filename="Game.qml" line="126"/>
-        <source>Low difficulty</source>
-        <translation>Низкая сложность</translation>
-    </message>
-    <message>
-        <location filename="Game.qml" line="126"/>
-        <source>High difficulty</source>
-        <translation>Высокая сложность</translation>
-    </message>
-    <message>
-        <location filename="Game.qml" line="137"/>
-        <source>White is moving</source>
-        <translation>Ходят белые</translation>
-    </message>
-    <message>
-        <location filename="Game.qml" line="137"/>
         <source>Black is moving</source>
         <translation>Ходят черные</translation>
     </message>
     <message>
-        <location filename="Game.qml" line="202"/>
-        <location filename="Game.qml" line="206"/>
-        <source>You won!</source>
-        <translation>Вы выиграли!</translation>
-    </message>
-    <message>
-        <location filename="Game.qml" line="202"/>
-        <location filename="Game.qml" line="206"/>
-        <source>You lose</source>
-        <translation>Вы проиграли</translation>
-    </message>
-    <message>
-        <location filename="Game.qml" line="241"/>
-        <source>Help</source>
-        <translation>Помощь</translation>
-    </message>
-    <message>
-        <location filename="Game.qml" line="268"/>
-        <source>Start with white</source>
-        <translation>Начать белыми</translation>
-    </message>
-    <message>
-        <location filename="Game.qml" line="295"/>
-        <source>Start with black</source>
-        <translation>Начать черными</translation>
-    </message>
-    <message>
-        <location filename="Game.qml" line="324"/>
-        <source>Hard</source>
-        <translation>Сложно</translation>
-    </message>
-    <message>
-        <location filename="Game.qml" line="324"/>
-        <source>Easy</source>
-        <translation>Легко</translation>
-    </message>
-    <message>
-        <location filename="Game.qml" line="371"/>
-        <source>White won!</source>
-        <translation>Победили белые!</translation>
-    </message>
-    <message>
-        <location filename="Game.qml" line="375"/>
         <source>Black won!</source>
         <translation>Победили черные!</translation>
     </message>
     <message>
-        <location filename="Game.qml" line="379"/>
+        <source>Continue</source>
+        <translation>Продолжить</translation>
+    </message>
+    <message>
         <source>Draw</source>
         <translation>Ничья</translation>
     </message>
     <message>
-        <location filename="Game.qml" line="438"/>
-        <source>Continue</source>
-        <translation>Продолжить</translation>
+        <source>Easy</source>
+        <translation>Легко</translation>
+    </message>
+    <message>
+        <source>Hard</source>
+        <translation>Сложно</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>Помощь</translation>
+    </message>
+    <message>
+        <source>High difficulty</source>
+        <translation>Высокая сложность</translation>
+    </message>
+    <message>
+        <source>Low difficulty</source>
+        <translation>Низкая сложность</translation>
+    </message>
+    <message>
+        <source>Reversi</source>
+        <translation>Реверси</translation>
+    </message>
+    <message>
+        <source>Start playing alone</source>
+        <translation>Играть одному</translation>
+    </message>
+    <message>
+        <source>Start playing together</source>
+        <translation>Играть вдвоем</translation>
+    </message>
+    <message>
+        <source>Start with black</source>
+        <translation>Начать черными</translation>
+    </message>
+    <message>
+        <source>Start with white</source>
+        <translation>Начать белыми</translation>
+    </message>
+    <message>
+        <source>White</source>
+        <translation>Белые</translation>
+    </message>
+    <message>
+        <source>White is moving</source>
+        <translation>Ходят белые</translation>
+    </message>
+    <message>
+        <source>White won!</source>
+        <translation>Победили белые!</translation>
+    </message>
+    <message>
+        <source>You lose</source>
+        <translation>Вы проиграли</translation>
+    </message>
+    <message>
+        <source>You won!</source>
+        <translation>Вы выиграли!</translation>
+    </message>
+    <message>
+        <source>You&apos;re playing with black</source>
+        <translation>Вы играете черными</translation>
+    </message>
+    <message>
+        <source>You&apos;re playing with white</source>
+        <translation>Вы играете белыми</translation>
     </message>
 </context>
 </TS>
