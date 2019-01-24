38847f8d528774ce90ce61dbc82061686c25bbd6
Ekaterina Samoilova <ekaterina.samoylova@gs-labs.ru>
2019-01-24 15:10:51 +0300

    [STB-22576]Tetris: localization added

---
+++ tetris/ru_RU.ts
@@ -0,0 +1,59 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>ExitMenu</name>
+    <message>
+        <source>Exit</source>
+        <translation>Выйти</translation>
+    </message>
+    <message>
+        <source>New game</source>
+        <translation>Новая игра</translation>
+    </message>
+    <message>
+        <source>Resume</source>
+        <translation>Продолжить игру</translation>
+    </message>
+</context>
+<context>
+    <name>GameOverMenu</name>
+    <message>
+        <source>Game over</source>
+        <translation>Игра окончена</translation>
+    </message>
+    <message>
+        <source>Play again</source>
+        <translation>Поиграть ещё</translation>
+    </message>
+    <message>
+        <source>Exit</source>
+        <translation>Выйти</translation>
+    </message>
+</context>
+<context>
+    <name>InfoRect</name>
+    <message>
+        <source>Score</source>
+        <translation>Счёт</translation>
+    </message>
+    <message>
+        <source>Level</source>
+        <translation>Уровень</translation>
+    </message>
+</context>
+<context>
+    <name>LevelMenu</name>
+    <message>
+        <source>Select level</source>
+        <translation>Выберите уровень</translation>
+    </message>
+</context>
+<context>
+    <name>PauseRect</name>
+    <message>
+        <source>Pause...</source>
+        <translation>Пауза...</translation>
+    </message>
+</context>
+</TS>
