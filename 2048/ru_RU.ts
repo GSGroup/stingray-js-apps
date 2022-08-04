ffdbe1f34f23bcdca134c35012699e03e4276390
Aleksandr Dikanchukov <aleksandr.dikanchukov@gs-labs.ru>
2022-08-04 15:18:15 +0300

    apps/2048: Add missing translations
    And sort them all alphabetically

--- 2048/ru_RU.ts
+++ 2048/ru_RU.ts
@@ -1,19 +1,15 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="ru_RU" sourcelanguage="ru_RU">
+<TS version="2.0" language="ru_RU">
 <context>
     <name>Game</name>
     <message>
-        <source>Game over</source>
-        <translation>Игра окончена</translation>
-    </message>
-    <message>
-        <source>You win!</source>
-        <translation>Вы выиграли!</translation>
-    </message>
-    <message>
         <source>BEST</source>
         <translation>ЛУЧШИЙ</translation>
+    </message>
+    <message>
+        <source>Game over</source>
+        <translation>Игра окончена</translation>
     </message>
     <message>
         <source>SCORE</source>
@@ -26,6 +22,23 @@
     <message>
         <source>Use your arrow keys to move the tiles. When two tiles with the same number touch, they merge into one!</source>
         <translation>Используйте стрелки, чтобы передвигать плитки. Когда две плитки с одинаковыми числами встречаются, они объединяются!</translation>
+    </message>
+    <message>
+        <source>You win!</source>
+        <translation>Вы выиграли!</translation>
+    </message>
+    <name>MenuDelegate</name>
+    <message>
+        <source>Continue</source>
+        <translation>Продолжить</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>Помощь</translation>
+    </message>
+    <message>
+        <source>New game</source>
+        <translation>Новая игра</translation>
     </message>
 </context>
 </TS>
